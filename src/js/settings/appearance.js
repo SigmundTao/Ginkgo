d5600a753ade5f4c647197a8216b60e9e9e50db5
Tao Simon <sigmundTao@gmail.com>
2026-06-29 18:41:58 +0100

    update dashboard input and improve settings menu

--- src/js/settings/appearance.js
+++ src/js/settings/appearance.js
@@ -1,22 +1,33 @@
 import { USER, updateUserData } from '../user.js'
 
+const defaultURL = './src/assets/kangae-logo.svg';
+
 export function createAppearanceMenu(){
   const containerEl = document.createElement('div')
   containerEl.classList.add('appearance-settings-el');
 
-  const title = document.createElement('p');
+  const title = document.createElement('h3');
   title.textContent = 'Dashboard Image';
 
-  const input = document.createElement('input');
-  input.value = USER.settings.appearance.dashboardLogo;
+  const description = document.createElement('p');
+  description.textContent = 'Paste in a url and hit enter, or type "default" for our default image:';
 
-  input.addEventListener('change', () => {
-    USER.settings.appearance.dashboardLogo = input.value;
+  const input = document.createElement('input');
+  let savedValue = USER.settings.appearance.dashboardLogo;
+  if(savedValue === defaultURL) savedValue = 'default';
+  input.value = savedValue;
+
+  input.addEventListener('keydown', (e) => {
+    if(!e.key === 'Enter') return;
+    
+    if(input.value === 'defaultURL') {
+      USER.settings.appearance.dashboardLogo = defaultURL;
+    } else {
+      USER.settings.appearance.dashboardLogo = input.value
+    }
     updateUserData()
   })
 
-  containerEl.appendChild(title)
-  containerEl.appendChild(input)
-
+  containerEl.append(title, description, input)
   return containerEl;
 }
